1c5e8b32d1a7abc93227bc39b636aad3fa9dde22
nishadAvnish <avnishkumar1st@gmail.com>
2025-09-08 21:02:57 +0530

    refactor: code refactor for test file

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,7 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tddcalc/string_calculator.dart';
 
 void main() {
-  StringCalculator calculator = StringCalculator();
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
 
   test("return 0 incase of empty string input", () {
     expect(calculator.addNumber(""), equals(0));
