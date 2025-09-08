e17eff003e2bb7fe18847b7d669bf9a54ebdac81
nishadAvnish <avnishkumar1st@gmail.com>
2025-09-08 21:34:53 +0530

    feat: input with custom delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,7 +4,14 @@ class StringCalculator {
       return 0;
     }
 
-    List<String> numList = numbers.split(RegExp(r'[, \n]'));
+    String delimiter = r'[, \n]';
+    if (numbers.startsWith("//")) {
+      int newLineIndex = numbers.indexOf("\n");
+      delimiter = numbers.substring(2, newLineIndex);
+      numbers = numbers.substring(newLineIndex + 1);
+    }
+
+    List<String> numList = numbers.split(RegExp(delimiter));
     List<int> parsedList = numList.map((ele) => int.parse(ele)).toList();
     int sum = parsedList.reduce(
       (accumulator, currentEle) => accumulator + currentEle,
