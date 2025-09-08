f23d22ec2877bd3a3c76e870287b7fa23c0fe840
nishadAvnish <avnishkumar1st@gmail.com>
2025-09-08 22:27:12 +0530

    feat: custom delimiters with empty numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,14 +1,14 @@
 class StringCalculator {
   int addNumber(String numbers) {
-    if (numbers.isEmpty) {
-      return 0;
-    }
-
     String delimiter = r'[, \n]';
     if (numbers.startsWith("//")) {
       int newLineIndex = numbers.indexOf("\n");
       delimiter = numbers.substring(2, newLineIndex);
       numbers = numbers.substring(newLineIndex + 1);
+    }
+
+    if (numbers.isEmpty) {
+      return 0;
     }
 
     List<String> numList = numbers.split(RegExp(delimiter));
